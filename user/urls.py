3484bafeeed0ebb855289b84e166fe00b3ff24c3
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-02-14 15:46:02 +0500

    login form added

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ from .views import Login,Register
 
 urlpatterns = [
     path('', Login.as_view(), name='login'),
-    path('register/', Register.as_view(), name='register'),
+    path('register', Register.as_view(), name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,16 +2,45 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from .models import CustomUser
+from .forms import LoginForm,Registerform
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 # Create your views here.
 class Login(View):
     def get(self,request):
         return render(request,'authentication/login.html')
     def post(self,request):
-        return HttpResponse('post')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User.objects.filter(username=username,password=password)
+            # authnticate user
+            user = authenticate(request,username=username,password=password)
+            print(user)
+            if user is not None:
+                login(request,user)
+                # go to register page
+                return reverse_lazy('register')
+            else:
+                return HttpResponse('login failed')
+        return render(request,'authentication/login.html',{'form':form})
 
 class Register(View):
     def get(self,request):
         return render(request,'authentication/register.html')
     def post(self,request):
-        return HttpResponse('post')
+        form = Registerform(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            print(username,email,password)
+            user = CustomUser.objects.create(user=User.objects.create(username=username,email=email,password=password))
+            if user:
+                return HttpResponse('login')
+            else:
+                return HttpResponse('register failed')
