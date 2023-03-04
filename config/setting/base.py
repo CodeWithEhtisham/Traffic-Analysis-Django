fca37cd2a3c14a2c97f13381ab9de05be08f62c3
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-03-04 22:17:41 +0500

    static folder issue solved

--- config/setting/base.py
+++ config/setting/base.py
@@ -259,9 +259,9 @@ USE_TZ = True
 import os
 from posixpath import normpath
 STATIC_URL = '/static/'
-
-STATIC_ROOT = normpath(os.path.join(BASE_DIR, 'static'))
-print(STATIC_ROOT)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
