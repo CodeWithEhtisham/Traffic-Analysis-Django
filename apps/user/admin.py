caff5f14a8f919ed7c82a766d5b2eab26e961719
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-03-06 17:15:20 +0500

    customer model added into admin panel

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'image', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+
+    def username(self, obj):
+        return obj.user.username
+
+    def email(self, obj):
+        return obj.user.email
+
+    username.admin_order_field = 'user__username'
+    email.admin_order_field = 'user__email'
+
+admin.site.register(CustomUser, CustomUserAdmin)
