9f5ed70e3dc00be8d4d73340f826ca317d92db90
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-04-17 14:54:46 +0500

    prediction model added

--- apps/analysis/models.py
+++ apps/analysis/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# class Site(models.Model):
+#     name = models.CharField(max_length=255)
+#     url = models.CharField(max_length=255)
+
+class Stream(models.Model):
+    # site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    site_name = models.CharField(max_length=255)
+    stream_id = models.CharField(max_length=255)
+    stream_url = models.CharField(max_length=255)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True)
+
+class Image(models.Model):
+    stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
+    image_path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Object(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    label = models.CharField(max_length=255)
+    confidence = models.FloatField()
+    x_min = models.FloatField()
+    y_min = models.FloatField()
+    x_max = models.FloatField()
+    y_max = models.FloatField()
--- yoloModel/prediction.py
+++ yoloModel/prediction.py
@@ -1,64 +1,7 @@
 import cv2
 import numpy as np
 # import onnxruntime as ort
-import onnx
-import pycuda.driver as cuda
-import pycuda.autoinit
-# import tensorrt as trt
-import torch
+from ultralytics import YOLO
 
-def load_model():
-    # Check if a GPU is available
-    if torch.cuda.is_available():
-        # Load the TensorRT model
-        # model = trt.tensorrtapi.LoadPlan('best.plan')
-        pass
-    else:
-        # Load the ONNX model
-        model = onnx.load('best.onnx')
-
-    return model
-
-
-# Load the model
-model = load_model()
-
-# Prepare the input data
-img_path = 'photo6.jpg'
-img = cv2.imread(img_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.resize(img, (640, 640))
-input_data = img.transpose(2, 0, 1).astype(np.float32)
-input_data /= 255.0
-
-# Run inference on the input data
-if isinstance(model): #trt.tensorrtapi.ICudaEngine
-    # TensorRT inference
-    # Create a TensorRT execution context
-    context = model.create_execution_context()
-
-    # Allocate GPU memory for input and output tensors
-    input_shape = (1, 3, 640, 640)
-    input_binding = context.get_binding_shape(0)
-    input_size = np.prod(input_shape) * np.dtype(np.float32).itemsize
-    input_buf = cuda.mem_alloc(input_size)
-
-    output_binding = context.get_binding_shape(1)
-    output_size = np.prod(output_binding) * np.dtype(np.float32).itemsize
-    output_buf = cuda.mem_alloc(output_size)
-
-    # Copy input data to GPU memory
-    cuda.memcpy_htod(input_buf, input_data)
-
-    # Run inference
-    context.execute_v2(bindings=[int(input_buf), int(output_buf)])
-
-    # Copy output data to CPU memory
-    output_data = np.empty(output_binding, dtype=np.float32)
-    cuda.memcpy_dtoh(output_data, output_buf)
-else:
-    # ONNX inference
-    output_data = model.run(None, {'input': input_data})
-
-# Process the output data as needed
-print(output_data)
+model = YOLO('yoloModel/best.pt')
+results = model('yoloModel/prediction.py', save=True)
