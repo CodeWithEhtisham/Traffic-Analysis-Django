d2e736c9ed059da61e106cf36dcc43a1cfd66c30
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-05-05 16:26:37 +0500

    js apis created

---
+++ static/js/apis.js
@@ -0,0 +1,49 @@
+
+// const socket = io.connect('http://127.0.0.1:7000');
+// socket.on('connect', function () {
+//     console.log('connected');
+// });
+
+// socket.on('disconnect', function () {
+//     console.log('disconnected');
+// });
+
+
+// socket.on('frame', function (data) {
+//     document.getElementById('image').src = "data:image/png;base64," + data;
+// });
+
+// // ajax apis for getting data
+// setInterval(function () {
+//     $.ajax({
+//         method: "GET",
+//         url: "/apis/get_objects",
+//         success: function (data) {
+//             console.log(data);
+//             document.getElementById('today_count').innerHTML = data.today;
+//             document.getElementById('yesterday_count').innerHTML = data.yesterday;
+//         },
+//         error: function () {
+//             console.log("error on get_vehicle_counts");
+//         }
+//     });
+// }, 10000); // 30 seconds in milliseconds
+
+
+// // ajax apis for getting data
+// setInterval(function () {
+//     $.ajax({
+//         method: "GET",
+//         url: "/apis/get_vehicle_counts",
+//         success: function (data) {
+//             console.log(data);
+//             document.getElementById('today_count').innerHTML = data.today;
+//             document.getElementById('yesterday_count').innerHTML = data.yesterday;
+//         },
+//         error: function () {
+//             console.log("error on get_vehicle_counts");
+//         }
+//     });
+// }, 10000); // 30 seconds in milliseconds
+
+
